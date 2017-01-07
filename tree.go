22866a2e265167cc85615cff569bf66571e19eec
robertsben <roberts.ben@me.com>
2017-01-07 19:43:31 +0000

    Pull the common structs out

---
+++ tree.go
@@ -0,0 +1,30 @@
+package main
+
+type Attribute struct {
+	Label 	string
+	Value 	string
+}
+
+type Attributes []*Attribute
+
+type Element struct {
+	Parent		*Element
+	Children 	Elements
+	Label 		string
+	Attrs		Attributes
+	Data 		string
+}
+
+type Elements []*Element
+
+func (self *Element) AddAttribute(attr xml.Attr) {
+	self.Attrs = append(self.Attrs, &Attribute{Label: attr.Name.Local, Value: attr.Value})
+}
+
+func (self *Element) AddChild(child *Element) {
+	self.Children = append(self.Children, child)
+}
+
+func (self *Element) AddSelfToParentsChildren() {
+	self.Parent.AddChild(self)
+}
--- xml_decoder.go
+++ xml_decoder.go
@@ -8,41 +8,6 @@ import (
 	"strings"
 )
 
-// <tree.go>
-
-type Attribute struct {
-	Label 	string
-	Value 	string
-}
-
-type Attributes []*Attribute
-
-type Element struct {
-	Parent		*Element
-	Children 	Elements
-	Label 		string
-	Attrs		Attributes
-	Data 		string
-}
-
-type Elements []*Element
-
-func (self *Element) AddAttribute(attr xml.Attr) {
-	self.Attrs = append(self.Attrs, &Attribute{Label: attr.Name.Local, Value: attr.Value})
-}
-
-func (self *Element) AddChild(child *Element) {
-	self.Children = append(self.Children, child)
-}
-
-func (self *Element) AddSelfToParentsChildren() {
-	self.Parent.AddChild(self)
-}
-
-// </tree.go>
-
-
-
 type Decoder struct {
 	r 	io.Reader
 	err error
